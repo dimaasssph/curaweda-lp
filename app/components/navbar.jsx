aba0a5a57d36231c37b203385703b1c6e3ebbecf
RifkySaputraMaylandra <andra04mei05@gmail.com>
2024-03-04 17:26:13 +0700

    test-responsive

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,16 +1,8 @@
-"use client"
 import Link from "next/link"
 import Image from "next/image"
-import { useEffect } from "react"
-import { usePathName } from "next/navigation"
+// import { useEffect } from "react"
 
 export const Navbar = ({ menus, logo }) => {
-    const pathname = usePathName
-
-    useEffect(() => {
-        console.log(pathname)
-    }, [])
-
     return (
         <>
             <div className="w-full p-3 bg-[#3C7083]" key="navbar-top" />
@@ -18,7 +10,7 @@ export const Navbar = ({ menus, logo }) => {
                 <Image src={logo} width={180} height={50} />
                 <div className="flex mt-2 ml-auto mr-10 space-x-8 align-middle">
                     {menus.map((menu, index) => (
-                        <Link href={menu.link} >
+                        <Link href={menu.link} key={index}>
                             <h1 key={`menu-${index}`} className="font-sans text-xl transition-all duration-300 ease-in-out hover:-translate-y-1 text-slate-950 hover:text-slate-500 hover:scale-125">
                                 {menu.title}
                             </h1>
