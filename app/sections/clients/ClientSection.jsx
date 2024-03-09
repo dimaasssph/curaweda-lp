5cce92436bae4b70b9653059dadbc2c770c6c5ce
RifkySaputraMaylandra <andra04mei05@gmail.com>
2024-03-09 15:21:31 +0700

    fix build

--- app/sections/clients/ClientSection.jsx
+++ app/sections/clients/ClientSection.jsx
@@ -5,9 +5,9 @@ export const ClientSection = ({ data, seenOnMedia }) => {
             <h1 className=" text-center text-4xl font-bold text-[#3C7083]">Our Clients & Partners</h1>
             {/* </div> */}
             <div className="w-full p-[100px] flex flex-wrap flex-grow align-middle justify-center space-x-3 space-y-3">
-                {data.map((image) => (
-                    <figure class="max-w-lg">
-                        <img class="h-auto w-28 rounded-lg" src={image} alt="image description" />
+                {data.map((image, index) => (
+                    <figure class="max-w-lg" key={index}>
+                        <img key={index} class="h-auto w-28 rounded-lg" src={image} alt="image description" />
                     </figure>
                 ))}
             </div>
@@ -15,9 +15,9 @@ export const ClientSection = ({ data, seenOnMedia }) => {
             <h1 className="text-4xl font-bold text-[#3C7083] text-center">Our Clients & Partners</h1>
             {/* </div> */}
             <div className="w-full p-[100px] flex flex-wrap flex-grow align-middle justify-center space-x-3 space-y-3">
-                {seenOnMedia.map((image) => (
-                    <figure class="max-w-lg">
-                        <img class="h-auto w-28 rounded-lg" src={image} alt="image description" />
+                {seenOnMedia.map((image, index) => (
+                    <figure class="max-w-lg" key={index}>
+                        <img key={index} class="h-auto w-28 rounded-lg" src={image} alt="image description" />
                     </figure>
                 ))}
             </div>
