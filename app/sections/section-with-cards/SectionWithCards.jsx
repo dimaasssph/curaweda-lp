cc6c8f048d9d14bc0123bceb444ae004d92af92f
RifkySaputraMaylandra <andra04mei05@gmail.com>
2024-03-12 06:18:11 +0700

    add animation

--- app/sections/section-with-cards/SectionWithCards.jsx
+++ app/sections/section-with-cards/SectionWithCards.jsx
@@ -1,6 +1,13 @@
+"use client"
 import { Card } from "../../components/card/card"
+import Aos from "aos"
+import "aos/dist/aos.css"
+import { useEffect } from "react"
 
 export const SectionWithCards = ({ title, items, shadow = false, textCenter = false, unique_id }) => {
+    useEffect(() => {
+        Aos.init()
+    })
     return (
         <div className="p-[50px] bg-[#D7E0DF] mb-5 w-full" id={unique_id}>
             <h1 className="text-3xl font-bold text-center text-black">
@@ -9,9 +16,10 @@ export const SectionWithCards = ({ title, items, shadow = false, textCenter = fa
             <hr className="w-full h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700" />
             <div className="flex flex-wrap justify-center space-x-5 align-middle">
                 {items.map((item, index) => (
-                    <Card key={index} item={item} shadow={shadow} textCenter={textCenter} />
+                    <Card key={index} item={item} shadow={shadow} textCenter={textCenter} duration={(index + 1) * 100 + 3000} />
                 ))}
             </div>
+
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,9 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-
+    "./src/*.{html,js}",
   ],
+
   theme: {
     extend: {
       backgroundImage: {
