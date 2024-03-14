85bc6c10784d54f28dc1a6ce21bef55d80737395
RifkySaputraMaylandra <andra04mei05@gmail.com>
2024-03-14 07:19:24 +0700

    add size client section

--- app/data/clients.js
+++ app/data/clients.js
@@ -1,27 +1,102 @@
-export const clients = [
-    '/assets/bank-bjb-1.png',
-    '/assets/btp-1.png',
-    '/assets/but-1.png',
-    '/assets/gnce-1.png',
-    '/assets/image-99.png',
-    '/assets/image-removebg-preview-2023-10-02t162136-2.png',
-    '/assets/itb-1.png',
-    '/assets/kesultanan-cirebon-1.png',
-    '/assets/logo-res-3.png',
-    '/assets/lpik-1.png',
-    '/assets/logo-smagaris-4.png',
-    '/assets/paylabs-1.png',
-    '/assets/pikiran-rakyat-1.png',
-    '/assets/sad-1.png',
-    '/assets/solelands-1.png',
-    '/assets/smk-13-1.png',
-    '/assets/smk-4-1.png',
-    '/assets/telu-1.png',
-    '/assets/tgh-1.png',
+export const clients = [{
+        image: '/assets/bank-bjb-1.png',
+        w: 150,
+        h: 80
+    },
+    {
+        image: '/assets/btp-1.png',
+        w: 113.42,
+        h: 80
+    },
+    {
+        image: '/assets/but-1.png',
+        w: 320,
+        h: 80
+    },
+    {
+        image: '/assets/gnce-1.png',
+        w: 320,
+        h: 80
+    },
+    {
+        image: '/assets/itb-1.png',
+        w: 80,
+        h: 80
+    },
+    {
+        image: '/assets/kesultanan-cirebon-1.png',
+        w: 77,
+        h: 80
+    },
+    {
+        image: '/assets/logo-res-3.png',
+        w: 265,
+        h: 80
+    },
+    {
+        image: '/assets/lpik-1.png',
+        w: 427,
+        h: 80
+    },
+    {
+        image: '/assets/paylabs-1.png',
+        w: 120,
+        h: 80
+    },
+    {
+        image: '/assets/pikiran-rakyat-1.png',
+        w: 295,
+        h: 80
+    },
+    {
+        image: '/assets/sad-1.png',
+        w: 87,
+        h: 80
+    },
+    {
+        image: '/assets/solelands-1.png',
+        w: 200,
+        h: 80
+    },
+    {
+        image: '/assets/smk-13-1.png',
+        w: 81,
+        h: 80
+    },
+    {
+        image: '/assets/smk-4-1.png',
+        w: 81,
+        h: 80
+    },
+    {
+        image: '/assets/telu-1.png',
+        w: 65,
+        h: 80
+    },
+    {
+        image: '/assets/tgh-1.png',
+        w: 80,
+        h: 80
+    },
 ]
-export const seenOnMedia = [
-    '/assets/favicon-1.png',
-    '/assets/image-99.png',
-    '/assets/pikiranrakyat-1.png',
-    '/assets/rri-bandung-1.png',
+export const seenOnMedia = [{
+        image: '/assets/favicon-1.png',
+        w: 80,
+        h: 80
+    },
+    {
+        image: '/assets/image-99.png',
+        w: 284,
+        h: 80
+    },
+    {
+        image: '/assets/pikiranrakyat-1.png',
+        w: 320,
+        h: 80
+    },
+    {
+        image: '/assets/rri-bandung-1.png',
+        w: 94,
+        h: 80
+    },
 ]
