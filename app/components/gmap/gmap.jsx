0343f8d72e822c90636bd58aef0b830218fb9649
Dimas Pramudia H <dimaspramudia0382@gmail.com>
2024-03-20 09:59:17 +0700

    gmap

--- app/components/gmap/gmap.jsx
+++ app/components/gmap/gmap.jsx
@@ -1,5 +1,3 @@
-export const GoogleMap;
-
 const GoogleMap = () => (
     <div className="h-full px-3 mapouter">
         <div className="gmap_canvas">
@@ -7,13 +5,12 @@ const GoogleMap = () => (
                 className="gmap_iframe"
                 width="100%"
                 height="100%"
-                src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=-6.917271969752331, 107.67116388876387&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-                    
-                </iframe>
+                src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=-6.917271969752331, 107.67116388876387&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+
+            ></iframe>
             <a href="https://embed-googlemap.com">embed google maps in website</a>
         </div>
     </div>
 );
 
 export default GoogleMap;
-
